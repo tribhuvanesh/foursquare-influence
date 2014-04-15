7dcc42da52e7c74f112eb5e144b0df90a66d284e
Siddharth Sarda <siddharthsarda01@gmail.com>
2014-04-15 13:37:55 +0200

    Adding a folder for preparing data. This extracts users from a checkins file. constructs friendships and alo lists users which have no friends

---
+++ data_prep/utils.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 sids <sids@siddharth-dev>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Set of functions I will need again and again
+"""
+
+
+def read_csv_file_and_return_tuples(filename):
+    tuples = []
+    with open(filename, "rb") as f:
+        for line in f.readlines():
+            line = line.strip()
+            tuples.append(line.split(','))
+    return tuples
+
+
+def get_unconnected_users():
+    fnofriends = open('no_friends.out', 'r')
+    no_friends = {}
+    for line in fnofriends:
+        no_friends[int(line.strip())] = 1
+    return no_friends
+
+
+def get_friendship_edges_dict(filename):
+    tuples = read_csv_file_and_return_tuples(filename)
+    edges = {}
+    for t in tuples:
+        u, v = t
+        #u, v = (min(u, v), max(u, v))
+        edges[(u, v)] = edges.setdefault((u, v), 0) + 1
+    return edges
