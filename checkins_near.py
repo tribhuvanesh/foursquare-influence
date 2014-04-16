7e31b1797fd7801d1f2179a1b7cff07ad6c97c4c
Siddharth Sarda <siddharthsarda01@gmail.com>
2014-04-16 12:58:56 +0200

    Get data for all locations in one pass. If you need an extra location, add its co-ordinates and also to the dictionary

--- checkins_near.py
+++ checkins_near.py
@@ -7,7 +7,12 @@ SF = (37.733436, -122.4343381)
 London = (51.5005135, -0.1274967)
 Paris = (48.821236, 2.271731)
 Rome = (41.8637027, 12.4697898)
-RADIUS = 100 # in kilometers
+RADIUS = 100  # in kilometers
+
+fout_dict = {Zurich: open("Zurich_checkins.csv", 'w'), SF: open("SF_checkins.csv", 'w'),
+             London: open("London_checkins.csv", 'w'), Paris: open("Paris_checkins.csv", 'w'),
+             Rome: open("Rome_checkins.csv", 'w')}
+
 
 # Enter location here from above
 NEAR_LOCATION = Paris
@@ -15,9 +20,9 @@ NEAR_LOCATION = Paris
 # Other constants
 WCL = 2290997
 
+
 def main():
-    fin = open('FoursquareCheckins20110101-20111231.csv')
-    fout = open('FoursquareCheckins20110101-20111231_near_paris.csv', 'w')
+    fin = open('SHTiesData/FoursquareCheckins.csv')
 
     # Fancy progress bar
     my_prperc = pyprind.ProgPercent(WCL)
@@ -29,14 +34,15 @@ def main():
         vals = line[:-2].split(',')
         lat, long = map(float, [vals[1], vals[2]])
         this_location = (lat, long)
-        if vincenty(NEAR_LOCATION, this_location) <= RADIUS:
-            fout.write(line[:-2])
-            fout.write('\n')
+        for loc, fout in fout_dict.iteritems():
+            if vincenty(loc, this_location) <= RADIUS:
+                fout.write(line[:-2])
+                fout.write('\n')
         my_prperc.update()
 
     fin.close()
-    fout.close()
-
+    for loc, fout in fout_dict.iteritems():
+        fout.close()
 
 if __name__ == '__main__':
     main()
